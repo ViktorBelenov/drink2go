e766289bce5ddb6e27c5ab2aca9dc12e2a94aaa9
Vitya <82025167+ViktorBelenov@users.noreply.github.com>
2023-06-15 01:02:08 +0300

    burger

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,26 @@
-/* в этот файл добавляет скрипты*/
+const navHeader = document.querySelector('.header-navigation');
+const navToggle = document.querySelector('.header-navigation__burger');
+
+navHeader.classList.remove('header-navigation--no-js');
+navHeader.classList.add('header-navigation--closed');
+
+
+const changeIcon = (element, icon) => {
+  const use = element.querySelector('use');
+  use.setAttribute('xlink:href', icon)
+}
+
+
+
+navToggle.addEventListener('click', function () {
+  if (navHeader.classList.contains('header-navigation--closed')) {
+    navHeader.classList.remove('header-navigation--closed');
+    navHeader.classList.add('header-navigation--opened');
+    changeIcon(navToggle, 'img/icons/stack.svg#burgerClosed')
+  } else {
+    navHeader.classList.add('header-navigation--closed');
+    navHeader.classList.remove('header-navigation--opened');
+
+    changeIcon(navToggle, 'img/icons/stack.svg#burger')
+  }
+});
