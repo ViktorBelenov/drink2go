c226ef41f75dc000f7a8bc21a01cd3883acb1a9e
Vitya <82025167+ViktorBelenov@users.noreply.github.com>
2023-06-20 03:35:54 +0300

    map

--- source/js/main.js
+++ source/js/main.js
@@ -21,3 +21,19 @@ const swiper = new Swiper('.swiper', {
     // },
 });
 console.log(swiper);
+  // Инициализация карты
+var map = L.map('map').setView([59.968137, 30.316272], 25);
+
+  // Добавление тайлового слоя карты
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+  // Создание и добавление кастомного пина
+var customIcon = L.icon({
+    iconUrl: '../img/map/map-pin.svg',
+    iconSize: [38, 50], // размеры иконки
+    iconAnchor: [19, 50], // якорь иконки (центр нижней точки)
+});
+
+var marker = L.marker([59.968137, 30.316272], { icon: customIcon }).addTo(map);
